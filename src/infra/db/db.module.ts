3317a50e0bfe8d8dd22c5d46f52befc239a29de6
JhonataOliveiraTeixeiraJhonataOliveiraTeixeira <jonasdeot@gmail.com>
2025-11-13 16:57:07 -0300

    Add CertificateDB

--- src/infra/db/db.module.ts
+++ src/infra/db/db.module.ts
@@ -1,11 +1,12 @@
 import { Module } from "@nestjs/common";
 import { UserDB } from "./user.db";
 import { PrismaModule } from "./prisma/prisma.module";
+import { CertificateDb } from "./certificate.db";
 
 
 @Module({
-  providers: [ UserDB ],
-  exports: [UserDB], 
+  providers: [UserDB, CertificateDb],
+  exports: [UserDB, CertificateDb], 
   imports: [PrismaModule]
 })
 export class DBModule {}
