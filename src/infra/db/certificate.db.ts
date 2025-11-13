559872588e83d1831bb923b84cb0274ba8229cae
JhonataOliveiraTeixeiraJhonataOliveiraTeixeira <jonasdeot@gmail.com>
2025-11-13 16:56:52 -0300

    Add new props in query for selection certificates in database

--- src/infra/db/certificate.db.ts
+++ src/infra/db/certificate.db.ts
@@ -1,13 +1,27 @@
+import { Injectable } from "@nestjs/common";
 import  { PrismaService } from "./prisma/prisma.service";
 
+@Injectable()
 export class CertificateDb {
  
   constructor(private readonly prisma: PrismaService) { } 
-  public getCertificatesByUserId(userId: string) {
-    return this.prisma.certificate.findMany({
+  public async getCertificatesByUserId(userId: string) {
+    return await this.prisma.certificate.findMany({
       where: {
         ownerId: userId
-      }
+      },
+      include: {
+        annex: {
+          select: {
+            publicUrl: true,
+            contentType: true
+          } 
+        },
+        owner: {
+          select: { name: true }
+        }
+      },
+      orderBy: { submittedAt: 'desc' },
     });
   }
 }
